7897c350773a7872921aab37690d4cc5ef415531
vladiusovich <vladiusovich@gmail.com>
2026-03-26 00:36:02 +0400

    refactor: fix auth store - add info on welcome page

--- src/lib/api/core/httpClient/interceptors/unauthorizeRedirect.ts
+++ src/lib/api/core/httpClient/interceptors/unauthorizeRedirect.ts
@@ -2,13 +2,12 @@ import { ACCESS_TOKEN, ORDER_HASH, REFRESH_TOKEN } from "$lib/const/authConstans
 import { goto } from "$app/navigation";
 import type { AxiosError } from "axios";
 
-type InterceptorFuncType = (value: any) => Promise<any>;
-
-export const unauthorizeRedirect: InterceptorFuncType = (error: AxiosError) => {
+export const unauthorizeRedirect = (error: AxiosError) => {
     if (error.response?.status === 401) {
         localStorage.removeItem(ACCESS_TOKEN);
         localStorage.removeItem(REFRESH_TOKEN);
         localStorage.removeItem(ORDER_HASH);
+
         goto("/welcome");
     }
     return Promise.reject(error);
--- src/routes/welcome/+page.svelte
+++ src/routes/welcome/+page.svelte
@@ -1,5 +1,15 @@
-<h2>Welcome to our service</h2>
-<p>
-    You should to scan QR-code of your order and only after that you will see
-    the order online
-</p>
+<script>
+    import BranchInfo from "$lib/components/shared/branchInfo/BranchInfo.svelte";
+    import ExecutionTimeInfo from "$lib/components/shared/executionTimeInfo/ExecutionTimeInfo.svelte";
+    import OrdersTable from "$lib/components/shared/ordersTable/OrdersTable.svelte";
+    import QueueInfo from "$lib/components/shared/queueInfo/QueueInfo.svelte";
+</script>
+
+<div class="flex flex-col items-start gap-2">
+    <h2 class="h2">Welcome to our service</h2>
+    <p class="text-xs">You should to scan QR-code of your order and only after that you will see the order online</p>
+
+    <ExecutionTimeInfo />
+    <QueueInfo />
+    <OrdersTable />
+</div>
