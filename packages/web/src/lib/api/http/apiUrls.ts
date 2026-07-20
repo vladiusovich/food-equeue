7dd50dee650169a06f5450666aa2cdaff6f6d617
vladiusovich <vladiusovich@gmail.com>
2026-07-20 23:40:51 +0400

    feat: env config for frontend app

--- packages/web/src/lib/api/http/apiUrls.ts
+++ packages/web/src/lib/api/http/apiUrls.ts
@@ -1,9 +1,10 @@
+import { PUBLIC_FOOD_SERVER_URL, PUBLIC_FOOD_SERVER_SOCKET_URL } from "$env/static/public";
+
 export type ApiUrlType = "foodServer" | "foodServerSocket";
 
-// TODO: config file?
 const apiUrls: Record<ApiUrlType, string> = {
-    foodServer: "http://192.168.100.11:3002",
-    foodServerSocket: "http://192.168.100.11:3002/customers",
+    foodServer: PUBLIC_FOOD_SERVER_URL,
+    foodServerSocket: PUBLIC_FOOD_SERVER_SOCKET_URL,
 };
 
 export default apiUrls;
